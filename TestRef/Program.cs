edca42bb3d139f73e79c23c4439d00f35ef966c5
Проколий Игорь Анатолиевич <dante.e3ghost@gmail.com>
2019-08-25 15:21:37 +0300

    Добавьте файлы проекта.

---
+++ TestRef/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRef
+{
+    class Program
+    {
+        static void RefExample(ref double a, ref double b)
+        {
+            double c;
+            c = a;
+            a = Math.Sqrt(b);
+            b = c;
+        }
+        static void Main(string[] args)
+        {
+            double x = 10, y = 34;
+
+            RefExample(ref x, ref y);
+            Console.WriteLine(x + " " + y);
+            Console.ReadLine();
+
+        }
+    }
+}
